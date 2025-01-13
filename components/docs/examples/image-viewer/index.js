8b1b4d79405639dd80ba1b2cc60774505bd60e4e
Akash Gautam <akash3gtm@gmail.com>
2025-01-13 13:08:02 +0530

    chore: added image-viewer

---
+++ components/docs/examples/image-viewer/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Icon, CloseIcon } from "@/components/ui/icon";
+import { Pressable } from "@/components/ui/pressable";
+
+import {
+  ImageViewer,
+  ImageViewerImage,
+  ImageViewerCloseButton,
+  ImageViewerBackdrop,
+  ImageViewerContent,
+} from "@/components/ui/image-viewer";
+
+import { Image } from "@/components/ui/image";
+import { Center } from "@/components/ui/center";
+export const examples = [
+  {
+    name: "Basic ImageViewer",
+    Code: function App() {
+      const [visible, setVisible] = React.useState(false);
+      const Images = [{ id: 1, url: "https://picsum.photos/1000/1000" }];
+      return (
+        <Center>
+          <Pressable onPress={() => setVisible(true)}>
+            <Image
+              source={{ uri: Images[0].url }}
+              className="w-[200px] h-[200px]"
+              resizeMode="contain"
+            />
+          </Pressable>
+          <ImageViewer isOpen={visible} onClose={() => setVisible(false)}>
+            <ImageViewerBackdrop>
+              <ImageViewerContent
+                images={Images}
+                renderImages={({ item, index }) => (
+                  <ImageViewerImage source={{ uri: item.url }} />
+                )}
+                keyExtractor={(item, index) => item.id + "-" + index}
+              >
+                <ImageViewerCloseButton>
+                  <Icon as={CloseIcon} />
+                </ImageViewerCloseButton>
+              </ImageViewerContent>
+            </ImageViewerBackdrop>
+          </ImageViewer>
+        </Center>
+      );
+    },
+  },
+];
